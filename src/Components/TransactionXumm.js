5c2eb754606d79c1604923fcb8bbed309f2948b5
QWindo <qwindo13@gmail.com>
2022-09-07 21:45:50 +0100

    Login changes

--- src/Components/TransactionXumm.js
+++ src/Components/TransactionXumm.js
@@ -1,71 +1,82 @@
 import {
-    Box,
-    Button,
-    Flex,
-    Link,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Text,
-    Center,
-    Stack,
-    useDisclosure,
-    Image,
-    ChakraProvider
-  } from "@chakra-ui/react";
-  import { ExternalLinkIcon, CopyIcon } from "@chakra-ui/icons";
+  Box,
+  Button,
+  Flex,
+  Link,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  Center,
+  Stack,
+  useDisclosure,
+  Image,
+  ChakraProvider
+} from "@chakra-ui/react";
+import { ExternalLinkIcon, CopyIcon } from "@chakra-ui/icons";
 
-  const ModalHeaderClass = {
-    borderBottom: '1px solid #2D2D2D',
+const ModalHeaderClass = {
+  borderBottom: '1px solid #2D2D2D',
+  textAlign: 'center',
+  fontSize: '16px',
+  background: '#262626',
+  color: 'white'
+};
+
+const ModalBodyClass = {
+  color: 'white'
+}
+
+const smallText = {
+  textAlign: 'center',
+  fontSize: '16px',
+  marginTop: '2rem',
+  marginBottom: '2rem'
+};
+
+const titleText = {
     textAlign: 'center',
-    fontSize: '16px',
-    background: '#262626',
-    color:'white'
-  };
+    fontSize: '22px',
+    marginTop: '2rem',
+    marginBottom: '2rem'
+}
 
-  const ModalBodyClass = {
-    background: '#2d2d2d',
-    color:'white'
+const qrCode = {
+    height: '350px'
+}
+
+export default function TransactionXumm(props) {
+
+  function handleClose() {
+    props.closeModal();
   }
 
-  const smallText = {
-    textAlign: 'center',
-    fontSize: '12px'
-  };
-
-  export default function TransactionXumm(props) {
-
-    function handleClose() {
-        props.closeModal();
-      }
-    
-    return (
-      <>
-  <ChakraProvider>
-<Modal onClose={handleClose} size="sm" isOpen={props.isOpen}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader style={ModalHeaderClass}>Sign-In</ModalHeader>
-          <ModalCloseButton color="white"/>
-          <ModalBody style={ModalBodyClass}>
-          <Stack
-        direction={'column'}
-        spacing={3}
-        align={'center'}
-        alignSelf={'center'}
-        position={'relative'}>
-                          <Text>Sign into XUMM</Text>
-                          <Image src={props.txnPng} />
-          </Stack>
-          <Text style={smallText}>Scan the QR code with your XUMM App to proceed.</Text>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+  return (
+    <>
+      <ChakraProvider>
+        <Modal onClose={handleClose} size="lg" isOpen={props.isOpen} isCentered  motionPreset='scale'>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalCloseButton color="white" />
+            <ModalBody style={ModalBodyClass}>
+              <Stack
+                direction={'column'}
+                spacing={3}
+                align={'center'}
+                alignSelf={'center'}
+                position={'relative'}>
+                <Text style={titleText}>Sign into XUMM</Text>
+                <Image style={qrCode} src={props.txnPng} />
+              </Stack>
+              <Text style={smallText}>Scan the QR code with your XUMM App to proceed.</Text>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
       </ChakraProvider>
-      </>
-    )
-  }
+    </>
+  )
+}
