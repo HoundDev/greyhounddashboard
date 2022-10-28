f9bbfffe72b81e0e82e6e58f96b4fa68cb5bc0e9
QWindo <qwindo13@gmail.com>
2022-10-28 13:28:13 +0100

    access denied page

--- src/Components/accessDenied.js
+++ src/Components/accessDenied.js
@@ -4,7 +4,7 @@ import { Link, useSearchParams, useLocation } from "react-router-dom";
 
 require("dotenv").config();
 
-export default function nftDetails() {
+export default function comingSoon() {
 
     return (
         <div className="content-body">
@@ -12,11 +12,8 @@ export default function nftDetails() {
                 <div className="row no-gutter overflow-h">
                     <div className="container d-flex justify-content-center align-self-center">
                         <div className="con-wl">
-                            <h3>Connect wallet</h3>
-                            <span className="mt-3 fw-500">Please connect your XUMM Wallet to see this page</span>
-                            <div className="mt-3">
-                                <button className="btn btn-login">Connect Wallet</button>
-                            </div>
+                            <h3>Coming Soon</h3>
+                            <span className="mt-3 fw-500">We're currently working on this section. Stay tuned for something amazing!</span>
                         </div>
                     </div>
 
