b03486013b9497c0b51110a177bb56d11eb7733e
Aaditya Thakkar <aadityathakkar47@gmail.com>
2022-12-02 20:29:39 +0530

    typo 2

--- src/Components/nftDetails.js
+++ src/Components/nftDetails.js
@@ -28,7 +28,7 @@ export default function NftDetails(props) {
         let response = await fetch(url);
         let data = await response.json();
         // console.log(data);
-        setCollectionId("/nftExplore?collectionId=" + data.nft.Issuer)
+        setCollectionId("/nftCollection?collectionId=" + data.nft.Issuer)
         return data.nft.Owner;
     }
 
